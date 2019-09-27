ff8d0772250bec657f23ff5c655f3a14aafcd6d2
Yordan Yosef Ramirez Calzado <yramirec@everis.com>
2019-09-27 17:51:47 -0500

    Zuul Gateway update

--- src/main/java/com/everis/ZuulGatewayApplication.java
+++ src/main/java/com/everis/ZuulGatewayApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.everis.filter.ErrorFilter;
+import com.everis.filter.PostFilter;
+import com.everis.filter.PreFilter;
+import com.everis.filter.RouteFilter;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
@@ -12,6 +18,25 @@ public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+	
+	@Bean
+	public PreFilter preFilter() {
+	  return new PreFilter();
+	}
+	
+	@Bean PostFilter postFilter() {
+	  return new PostFilter();
+	}
+	
+	@Bean
+	public ErrorFilter errorFilter() {
+	  return new ErrorFilter();
+	}
+	
+	@Bean
+	public RouteFilter routeFilter() {
+	  return new RouteFilter();
 	}
 
 }
